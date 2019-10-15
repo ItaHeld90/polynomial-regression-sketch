81b8e6b790286c60899450b76146267aa81af155
Itamar Held <ita@gmail.com>
2019-10-15 20:25:07 +0300

    control by slider and numeric up and down

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,62 @@
 //@ts-ignore
 import P5Wrapper from 'react-p5-wrapper';
 import React from 'react';
+import { observer } from 'mobx-react';
+import { Slider, InputNumber } from 'antd';
 import { getSketch } from './sketch';
 import { RegressionCanvasState } from './regression-canvas-state';
 
+import 'antd/dist/antd.css';
+
 const canvasState = new RegressionCanvasState();
 
-function App() {
-    return (
-        <div className="App">
-            <P5Wrapper sketch={getSketch(canvasState)} />
-        </div>
-    );
+const minNumParams = 2;
+const maxNumParams = 20;
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <P5Wrapper sketch={getSketch(canvasState)} />
+                <AppControllers />
+            </div>
+        );
+    }
+}
+
+@observer
+class AppControllers extends React.Component {
+    render() {
+        return (
+            <div style={{ margin: 10 }}>
+                <div style={{ fontStyle: 'bold' }}>Number of Parameters:</div>
+                <div style={{ display: 'flex' }}>
+                    <div style={{ width: 200 }}>
+                        <Slider
+                            min={minNumParams}
+                            max={maxNumParams}
+                            value={canvasState.numParams}
+                            onChange={val => {
+                                typeof val === 'number' &&
+                                    canvasState.setNumThetas(val);
+                            }}
+                        />
+                    </div>
+                    <div style={{ width: 20 }}>
+                        <InputNumber
+                            min={minNumParams}
+                            max={maxNumParams}
+                            value={canvasState.numParams}
+                            onChange={val => {
+                                typeof val === 'number' &&
+                                    canvasState.setNumThetas(val);
+                            }}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/regression-canvas-state.ts
+++ src/regression-canvas-state.ts
@@ -1,20 +1,27 @@
 import * as tf from '@tensorflow/tfjs';
+import { computed, observable, action } from 'mobx';
 import { times } from 'lodash';
 import { randomInRange } from './helper-utils';
 
 const defaultNumParameters = 3;
 
 export class RegressionCanvasState {
-    thetas: tf.Variable<tf.Rank.R0>[] = times(defaultNumParameters, () =>
-        tf.scalar(randomInRange(-1, 1)).variable()
+    @observable.ref thetas: tf.Variable<tf.Rank.R0>[] = times(
+        defaultNumParameters,
+        () => tf.scalar(randomInRange(-1, 1)).variable()
     );
 
+    @action
     setNumThetas(numParams: number) {
+        // dispose old thetas
+        this.thetas.forEach(theta => theta.dispose());
+
         this.thetas = times(numParams, () =>
             tf.scalar(randomInRange(-1, 1)).variable()
         );
     }
 
+    @computed
     get numParams(): number {
         return this.thetas.length;
     }
