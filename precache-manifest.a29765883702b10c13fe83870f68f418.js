54f1fdef0426729d1f24e10f8213945ee662ee97
Itamar Held <ita@gmail.com>
2019-11-10 00:59:19 +0200

    init

---
+++ precache-manifest.a29765883702b10c13fe83870f68f418.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d698a000187f533792585906b35b734",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a79f814089256bf26a37",
+    "url": "/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "a79f814089256bf26a37",
+    "url": "/static/js/2.1b3639a7.chunk.js"
+  },
+  {
+    "revision": "0e2274ae57b021b4be2a",
+    "url": "/static/js/main.8b4a7a55.chunk.js"
+  },
+  {
+    "revision": "1f26bbb3abf37dffba44",
+    "url": "/static/js/runtime-main.e6216973.js"
+  }
+]);
