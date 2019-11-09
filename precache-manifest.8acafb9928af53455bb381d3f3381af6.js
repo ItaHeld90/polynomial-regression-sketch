5a229b8a07173cd550a6b1853301427844a12800
Itamar Held <ita@gmail.com>
2019-11-10 01:23:39 +0200

    Updates

---
+++ precache-manifest.8acafb9928af53455bb381d3f3381af6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b41ae69d9a2fb3555e30b046c10d1980",
+    "url": "/polynomial-regression-sketch/index.html"
+  },
+  {
+    "revision": "0f31f1164110ac4f2ae7",
+    "url": "/polynomial-regression-sketch/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "0f31f1164110ac4f2ae7",
+    "url": "/polynomial-regression-sketch/static/js/2.393f82d1.chunk.js"
+  },
+  {
+    "revision": "5af3426d9fb971072c8e",
+    "url": "/polynomial-regression-sketch/static/js/main.421c0b52.chunk.js"
+  },
+  {
+    "revision": "932f6ce23030cebd0e2b",
+    "url": "/polynomial-regression-sketch/static/js/runtime-main.954aeb22.js"
+  }
+]);
